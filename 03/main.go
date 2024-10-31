d2791d0dd7b6d9f12f6f3dc820d991d27f83f632
n0rrman <mail@henriknorrman.com>
2024-10-31 01:09:14 +0100

    day 03 data loaded

--- 03/main.go
+++ 03/main.go
@@ -7,18 +7,30 @@ import (
 	"strings"
 )
 
-func readData(file string) []string {
+type grid [][]bool
+
+func readData(file string) grid {
 	body, err := os.ReadFile(file)
 	if err != nil {
 		log.Fatalf("unable to read file: %v", err)
 	}
 	sData := strings.Split(string(body[:]), "\n")
 
-	return sData
+	g := make(grid, len(sData))
+	for y, row := range sData {
+		sRow := strings.Split(row, "")
+		g[y] = make([]bool, len(sRow))
+		for x, entry := range sRow {
+			g[y][x] = entry == "#"
+		}
+	}
+
+	return g
 }
 
 func main() {
 	data := readData("data")
+	fmt.Println(data)
 	_ = data
 
 	// Part One
