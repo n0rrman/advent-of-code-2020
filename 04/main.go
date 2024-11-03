82e1a823d7b7b21fc5abe26b759c10e20d88093f
n0rrman <mail@henriknorrman.com>
2024-11-03 14:38:07 +0100

    day 04 data loaded

--- 04/main.go
+++ 04/main.go
@@ -7,18 +7,57 @@ import (
 	"strings"
 )
 
-func readData(file string) []string {
+type passportData struct {
+	byr string //(Birth Year)
+	iyr string //(Issue Year)
+	eyr string //(Expiration Year)
+	hgt string //(Height)
+	hcl string //(Hair Color)
+	ecl string //(Eye Color)
+	pid string //(Passport ID)
+	cid string //(Country ID)
+}
+
+func readData(file string) []passportData {
 	body, err := os.ReadFile(file)
 	if err != nil {
 		log.Fatalf("unable to read file: %v", err)
 	}
-	sData := strings.Split(string(body[:]), "\n")
+	sData := strings.Split(string(body[:]), "\n\n")
 
-	return sData
+	pd := make([]passportData, len(sData))
+	for i, s := range sData {
+		data := strings.Split(strings.Replace(s, "\n", " ", -1), " ")
+		passportData := passportData{}
+		for _, dataType := range data {
+			dt := strings.Split(dataType, ":")
+			switch dt[0] {
+			case "byr":
+				passportData.byr = dt[1]
+			case "iyr":
+				passportData.iyr = dt[1]
+			case "eyr":
+				passportData.eyr = dt[1]
+			case "hgt":
+				passportData.hgt = dt[1]
+			case "hcl":
+				passportData.hcl = dt[1]
+			case "ecl":
+				passportData.ecl = dt[1]
+			case "pid":
+				passportData.pid = dt[1]
+			case "cid":
+				passportData.cid = dt[1]
+			}
+		}
+		pd[i] = passportData
+	}
+	return pd
 }
 
 func main() {
-	data := readData("data")
+	data := readData("test_data")
+	fmt.Println(data)
 	_ = data
 
 	// Part One
