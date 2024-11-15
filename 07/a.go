1a14f91dc3144e3724fbd3046d163fb8565bfe02
n0rrman <mail@henriknorrman.com>
2024-11-15 17:41:13 +0100

    day 07 part 1 completed

--- 07/a.go
+++ 07/a.go
@@ -1,1 +1,38 @@
 package main
+
+const (
+	NULL  = 0
+	FALSE = -1
+	TRUE  = 1
+)
+
+func countBagColours(start string, bags map[string]bag) int {
+	memo := map[string]int{}
+	for _, bag := range bags {
+		memo[bag.colour] = traverseBag(bag.colour, start, bags, &memo)
+	}
+
+	count := 0
+	for _, bag := range memo {
+		if bag == TRUE {
+			count++
+		}
+	}
+	return count
+}
+
+func traverseBag(start, end string, b map[string]bag, m *map[string]int) int {
+	if (*m)[start] != NULL {
+		return (*m)[start]
+	}
+
+	contained := b[start].bags
+	for _, c := range contained {
+		if c == end || traverseBag(c, end, b, m) == TRUE {
+			(*m)[start] = TRUE
+			return TRUE
+		}
+	}
+	(*m)[start] = FALSE
+	return FALSE
+}
