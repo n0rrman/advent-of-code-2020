b7f313100fe93f19828cebf1852145a9de3a8812
n0rrman <mail@henriknorrman.com>
2024-11-20 08:12:58 +0100

    day 08 part 1 completed

--- 08/a.go
+++ 08/a.go
@@ -1,5 +1,29 @@
 package main
 
-func a(_ any) int {
-	return 1
+func executeInstr(i instruction, pc, acc int) (int, int) {
+	switch i.InstrType {
+	case "acc":
+		return pc + 1, acc + i.Value
+	case "jmp":
+		return pc + i.Value, acc
+	default:
+		return pc + 1, acc
+	}
+}
+
+func executeProgram(program []instruction) int {
+	run := true
+	pc := 0
+	acc := 0
+
+	for run {
+		instr := program[pc]
+		if instr.Executed {
+			run = false
+			continue
+		}
+		program[pc].Executed = true
+		pc, acc = executeInstr(instr, pc, acc)
+	}
+	return acc
 }
