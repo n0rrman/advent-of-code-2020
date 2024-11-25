83638cb3bdd3d766c2ada160f571f3829d81df19
n0rrman <mail@henriknorrman.com>
2024-11-25 18:09:00 +0100

    day 10 init

---
+++ 10/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestA(t *testing.T) {
+	data := readData("test_data")
+	results := a(data)
+
+	const e = 1
+	if results != e {
+		t.Errorf("Expected %v, but got %v", e, results)
+	}
+}
+func TestA2(t *testing.T) {
+	data := readData("test_data2")
+	results := a(data)
+
+	const e = 1
+	if results != e {
+		t.Errorf("Expected %v, but got %v", e, results)
+	}
+}
+
+func TestB(t *testing.T) {
+	data := readData("test_data")
+	results := b(data)
+
+	const e = 2
+	if results != e {
+		t.Errorf("Expected %v, but got %v", e, results)
+	}
+}
