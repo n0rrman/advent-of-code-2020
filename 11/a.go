f0a41e5eb3fe1d4debfdfc31fd355a7fccaccb27
n0rrman <mail@henriknorrman.com>
2024-11-29 07:56:24 +0100

    day 11 part 1 completed

--- 11/a.go
+++ 11/a.go
@@ -1,6 +1,84 @@
 package main
 
-func calcOccupied(seats [][]int) int {
+func countAdjacent(s [][]int, y, x int) int {
+	count := 0
+	if x > 0 && s[y][x-1] == IN_USE {
+		count++
+	}
+	if x < len(s[0])-1 && s[y][x+1] == IN_USE {
+		count++
+	}
+	if y > 0 {
+		if s[y-1][x] == IN_USE {
+			count++
+		}
+		if x > 0 && s[y-1][x-1] == IN_USE {
+			count++
+		}
+		if x < len(s[0])-1 && s[y-1][x+1] == IN_USE {
+			count++
+		}
+	}
+	if y < len(s)-1 {
+		if s[y+1][x] == IN_USE {
+			count++
+		}
+		if x > 0 && s[y+1][x-1] == IN_USE {
+			count++
+		}
+		if x < len(s[0])-1 && s[y+1][x+1] == IN_USE {
+			count++
+		}
+	}
+	return count
+}
 
-	return 1
+func oneStep(s [][]int) ([][]int, bool) {
+	changed := false
+	newSeats := make([][]int, len(s))
+	for y := range s {
+		newSeats[y] = make([]int, len(s[y]))
+		for x := range s[y] {
+			numAdj := countAdjacent(s, y, x)
+			seat := s[y][x]
+			if seat == IN_USE && numAdj >= 4 {
+				newSeats[y][x] = EMPTY
+				changed = true
+			} else if seat == EMPTY && numAdj == 0 {
+				newSeats[y][x] = IN_USE
+				changed = true
+			} else {
+				newSeats[y][x] = seat
+			}
+		}
+	}
+	return newSeats, changed
+}
+
+func stabiliseSeats(s [][]int) [][]int {
+	seats := s
+	changed := true
+	for changed {
+		seats, changed = oneStep(seats)
+	}
+	return seats
+}
+
+func countSeats(s [][]int) int {
+	count := 0
+	for y := range s {
+		for x := range s[y] {
+			if s[y][x] == IN_USE {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func calcOccupied(seats [][]int) int {
+	seats = stabiliseSeats(seats)
+	count := countSeats(seats)
+
+	return count
 }
--- 11/main.go
+++ 11/main.go
@@ -7,23 +7,27 @@ import (
 	"strings"
 )
 
+const (
+	IN_USE     = 1
+	EMPTY      = -1
+	NOT_A_SEAT = 0
+)
+
 func readData(file string) [][]int {
 	body, err := os.ReadFile(file)
 	if err != nil {
 		log.Fatalf("unable to read file: %v", err)
 	}
 	sData := strings.Split(string(body[:]), "\n")
-
 	data := make([][]int, len(sData))
 
 	for y, row := range sData {
-		fmt.Println(sData[y])
 		data[y] = make([]int, len(row))
 		for x, char := range strings.Split(row, "") {
 			if char == "L" {
-				data[y][x] = -1
+				data[y][x] = EMPTY
 			} else {
-				data[y][x] = 0
+				data[y][x] = NOT_A_SEAT
 			}
 		}
 	}
